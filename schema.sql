bf796ddcd1703631bdb1001cf3407a34ef94a817
blase147 <solarmails2@gmail.com>
2022-12-22 12:43:52 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
-/* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of entire database.*/
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- Table: animals.animals
+
+-- DROP TABLE IF EXISTS animals.animals;
+
+CREATE TABLE IF NOT EXISTS animals.animals
+(
+    id integer,
+    name text COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg point
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS animals.animals
+    OWNER to postgres;
