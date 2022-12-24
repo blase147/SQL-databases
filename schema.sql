7f5a6b15b64bc3da12e97fd19efe5bcc8a98d9eb
blase147 <solarmails2@gmail.com>
2022-12-24 10:13:19 +0100

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -2,18 +2,58 @@
 
 -- DROP TABLE IF EXISTS public.animals;
 
-CREATE TABLE IF NOT EXISTS public.animals
+CREATE TABLE IF NOT EXISTS animals.animals
 (
-    id integer NOT NULL,
+    animals_id integer NOT NULL,
     name text COLLATE pg_catalog."default",
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
-    CONSTRAINT animals_pkey PRIMARY KEY (id)
+    PRIMARY KEY (animals_id),
 )
 
-TABLESPACE pg_default;
+-- Create a table named owners
+BEGIN;
+CREATE TABLE animals.owners (
+    id serial PRIMARY KEY,
+    full_name text,
+    age text
+)
+COMMIT;
 
-ALTER TABLE IF EXISTS public.animals
-    OWNER to postgres;
+-- Create a table named species
+BEGIN;
+CREATE TABLE animals.species (
+    species_id serial PRIMARY KEY,
+    name text,
+)
+COMMIT;
+
+-- Modify animals table
+-- id is set as autoincremented PRIMARY KEY
+BEGIN;
+
+ALTER TABLE animals.animals
+ADD serial PRIMARY KEY (animals_id)
+
+-- Remove column species
+ALTER TABLE animals.animals 
+DROP species
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals.animals 
+ADD species_id
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals.animals 
+ADD owner_id
+
+COMMIT;
+
+
+
+
+
+
+
